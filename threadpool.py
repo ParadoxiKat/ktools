ee4ecc42b2778edfa2d444ed14e8bcfba7866b86
Katie Durden <blackmagicgirl@users.noreply.github.com>
2017-09-25 12:02:30 -0700

    threadpool.py: Made use of queue module and range function python 2 and 3 independent.     Renamed _cmd to task_handler. Removed it as parameter to worker thread. Worker thread has a property that either returns the task_handler function from the pool, or a lambda that accepts all arguments and returns None if the thread or pool is dying.

--- threadpool.py
+++ threadpool.py
@@ -6,35 +6,44 @@
 
 from __future__ import division, print_function
 import logging
-import Queue
+try:
+	import Queue as queue
+except ImportError:
+	import queue
 import threading
+
+# Monkey patch range with xrange in Python2.
+try:
+	_range, range = range, xrange
+except NameError:
+	pass
 
 logger=logging.getLogger(__name__)
 
 class ThreadPool(object):
 	"""A dynamic thread pool to handle any data type."""
-	def __init__(self, cmd, num_threads=5, valid_types=()):
-		"""argument cmd should be a callable and num_threads an int (defaults to 5 
-		if not supplied). valid_types is a tuple of types that can be put into the Queue. If left empty 
+	def __init__(self, task_handler, num_threads=5, valid_types=()):
+		"""argument task_handler should be a callable and num_threads an int (defaults to 5 
+		if not supplied). valid_types is a tuple of types that can be put into the queue. If left empty 
 		all types are accepted (this is the default).
 		"""
-		self._cmd=cmd
+		self._task_handler=task_handler
 		#set the number of items the queue can hold to 5 times the threadcount
 		#I don't know why.
-		self._tasks=Queue.Queue(num_threads*5)
+		self._tasks=queue.queue(num_threads*5)
 		self._valid_types=valid_types
 		self._tasks_lock=threading.Condition(threading.Lock())
 		self._resize_lock=threading.Condition(threading.Lock())
 		self._is_dying=False
 		self._threads=set()
 		for x in range(num_threads):
-			new_thread=_WorkerThread(self, cmd)
+			new_thread=_WorkerThread(self)
 			new_thread.start()
 			self._threads.add(new_thread)
 
 	def insert_task(self, new_task):
 		"""
-		insert a new task into the Queue. If self._valid_types is empty, all types are accepted. 
+		insert a new task into the queue. If self._valid_types is empty, all types are accepted. 
 		Otherwise, the type of new_task must be in self._valid_types.
 		"""
 		logger.debug("Received {}.".format(new_task))
@@ -58,7 +67,7 @@ class ThreadPool(object):
 	def _get_task(self):
 		"""
 		FOR INTERNAL USE ONLY!!!
-		Gets and returns an item from the Queue, for use by worker threads.
+		Gets and returns an item from the queue, for use by worker threads.
 		"""
 		return self._tasks.get()
 
@@ -89,7 +98,7 @@ class ThreadPool(object):
 				elif n > len(self._threads):
 					#create new threads,start them, and add them to the pool.
 					for x in range(n-len(self._threads)):
-						new_thread=_WorkerThread(self, self._cmd)
+						new_thread=_WorkerThread(self, self._task_handler)
 						new_thread.start()
 						self._threads.add(new_thread)
 				else:
@@ -104,12 +113,16 @@ class ThreadPool(object):
 
 class _WorkerThread(threading.Thread):
 	"""Worker thread for use by the threadpool only!"""
-	def __init__(self, pool, cmd):
+	def __init__(self, pool):
 		#hang onto the pool object so we can check the queue and other things
 		self._pool=pool
-		self._cmd=cmd
 		self._alive=True
 		threading.Thread.__init__(self)
+
+	@property
+	def _task_handler(self):
+		if not self._alive or self._pool._is_dying: return lambda *a, **kw:None
+		return self._pool._task_handler
 
 	def run(self):
 		logger.debug("{} starting".format(self.getName()))
@@ -131,7 +144,7 @@ class _WorkerThread(threading.Thread):
 				logger.debug("{} dying.".format(self.getName()))
 				self._pool._threads.remove(self)
 				break
-			else: #Do our job and call cmd with task!
+			else: #Do our job and call task_handler with task!
 				logger.debug("{} processing task {}.".format(self.getName(), task))
-				self._cmd(self, self._pool, task)
+				self._task_handler(self, self._pool, task)
 
