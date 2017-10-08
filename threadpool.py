4cf855285e9c3e9f56a270f3d6c61537b6fe9e09
Katie Durden <blackmagicgirl@users.noreply.github.com>
2017-10-07 17:37:32 -0700

    threadpool.py: Typo/bugfix. In ThreadPool.stop reference self.tasks_queue, not q.

--- threadpool.py
+++ threadpool.py
@@ -128,7 +128,7 @@ class ThreadPool(object):
 		else:
 			#alive is False, we can't take new tasks
 			raise ThreadPoolDying
-		unfinished = len(tuple(tasks))
+		unfinished = len(tuple(new_tasks))
 		if unfinished: numtasks = '{} of {}'.format(inserted, inserted+unfinished)
 		else: numtasks=str(inserted)
 		logger.debug("Inserted {} tasks.".format(numtasks))
@@ -188,7 +188,7 @@ class ThreadPool(object):
 			for thread in self._threads.copy(): thread.stop()
 			#empty the queue:
 			while not self.tasks_queue.empty():
-				_ = q.get()
+				_ = self.tasks_queue.get()
 				self.tasks_queue.task_done()
 
 	def stop_thread(self, thread):
