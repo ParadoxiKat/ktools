05e13aaa572a10aad6bf239f577cccb9f9ece7db
Katie Durden <blackmagicgirl@users.noreply.github.com>
2017-11-04 17:55:19 -0700

    config.py: Config now takes any number of paths as arguments, and has a single keyword argument defaultfilename='config.json'.     If any path is a directory, defaultfilename is appended. If no paths are given, cwd is used.     Only the final file loaded can be modified.

--- config.py
+++ config.py
@@ -12,25 +12,42 @@ import threading
 # Local Modules:
 from .utils import get_program_path, get_settings_path
 
-class Error(Exception):
+config_lock = threading.RLock()
+
+class ConfigError(Exception):
 	"""Config Error"""
 
 class Config(collections.MutableMapping):
-	def __init__(self, prog_name='', name="config", subdir='.', *args, **kwargs):
-		super(Config, self).__init__(*args, **kwargs)
-		self.prog_name = prog_name
-		self._name = name
-		self._config = dict()
-		self.subdir = subdir
+	"""JSON config file parser
+	
+	Loads 1 or more json-formatted config files and presents them as a dictionary.
+	
+	Config(*files, defaultfilename='config.json')
+	
+	Files are loaded in order, left to right. If the path is a directory, defaultfilename is appended.
+	Read opperations start from the last file loaded and work their way back. So keys present in later files override the same key from an earlier file.
+	Write opperations only opperate on the final file given.
+	"""
+
+	def __init__(self, *args, **kwargs):
+		super(Config, self).__init__()
+		self.config_files = []
+		self._defaultfilename = kwargs.pop('filename', 'config.json')
+		for path in args:
+			path = os.path.abspath(path)
+			if os.path.exists(path):
+				if os.path.isdir(path): self.config_files.append(os.path.join(path, self._defaultfilename))
+				elif os.path.isfile(path): self.config_files.append(path)
+			else:
+				if os.path.isdir(os.path.dirname(path)): self.config_files.append(path)
+		if not self.config_files: self.config_files.append(os.path.join(os.getcwd(), self._defaultfilename))
 		self.reload()
 
 	@property
-	def name(self):
-		return self._name
-
-	@name.setter
-	def name(self, value):
-		self._name = value
+	def _config(self):
+		d={}
+		for c in self._configs: d.update(c)
+		return d
 
 	def _parse(self, file_name):
 		if os.path.exists(file_name):
@@ -39,39 +56,47 @@ class Config(collections.MutableMapping):
 					with codecs.open(file_name, "rb", encoding="utf-8") as file_object:
 						return json.load(file_object)
 				except IOError as e:
-					raise Error("{}: '{}'".format(e.strerror, e.filename))
+					raise ConfigError("{}: '{}'".format(e.strerror, e.filename))
 				except ValueError:
-					raise Error("Corrupted json file: {}".format(file_name))
+					raise ConfigError("Corrupted json file: {}".format(file_name))
 			else:
-				raise Error("'{}' is a directory, not a file.".format(file_name))
+				raise ConfigError("'{}' is a directory, not a file.".format(file_name))
 		else:
 			return {}
-
-	def reload(self):
-		self._config.clear()
-		self._config.update(self._parse(get_program_path(self.prog_name, self.subdir, '{}.json'.format(self.name))))
-		self._config.update(self._parse(get_settings_path(self.prog_name, self.subdir, '{}.json'.format(self.name))))
-
-	def save(self):
-		data_directory = get_settings_path(self.prog_name, self.subdir)
-		if not os.path.exists(data_directory): os.makedirs(data_directory)
-		file_name = os.path.join(data_directory, "{}.json".format(self._name))
-		with codecs.open(file_name, "wb", encoding="utf-8") as file_object:
-			json.dump(self._config, file_object, sort_keys=True, indent=2, separators=(",", ": "))
 
 	def __getitem__(self, key):
 		return self._config[key]
 
 	def __setitem__(self, key, value):
-		self._config[key] = value
+		self._configs[-1][key] = value
 
 	def __delitem__(self, key):
-		del self._config[key]
+		del self._configs[-1][key]
 
 	def __iter__(self):
 		return iter(self._config)
 
 	def __len__(self):
 		return len(self._config)
+	def clear(self):
+		return self._configs[-1].clear()
 
-config_lock = threading.Lock()
+	def pop(self, *a, **kw):
+		return self._configs[-1].pop(*a, **kw)
+
+	def popitems(self, *a, **kw):
+		return self._configs[-1].popitems(*a, **kw)
+
+	def reload(self):
+		self._configs = []
+		for f in self.config_files: self._configs.append(self._parse(f))
+
+	def save(self):
+		with codecs.open(self.config_files[-1], "wb", encoding="utf-8") as file_object:
+			json.dump(self._configs[-1], file_object, sort_keys=True, indent=2, separators=(",", ": "))
+
+	def setdefault(self, *a, **kw):
+		return self._configs[-1].setdefault(*a, **kw)
+
+	def update(self, *a, **kw):
+		return self._configs[-1].update(*a, **kw)
